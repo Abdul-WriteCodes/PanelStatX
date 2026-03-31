64a20fd2cc3d8382fe66d3707f8a98eb05a27d02
Abdul <Abdulwrite77@gmail.com>
2026-03-31 23:21:26 +0100

    improved the UI flow

--- app.py
+++ app.py
@@ -25,14 +25,15 @@ def check_auth():
         <p style='font-size:14px; color:#2ECC71; text-align:center; font-weight:600'>
             🎯 Upload Data • 📝 Select Model • 🔄 Analyse • ✅ Download Results
         </p>
-        
-       <p>
-        Supports Panel Regression for Different Datasets:<br><br>
-        🎯 <strong>Companies</strong><br>
-        📝 <strong>Countries</strong><br>
-        ⏱️ <strong>Industries</strong><br>
-        👇 <strong>Any panel data structure</strong><br>
-        
+    </div>
+    
+    <div style='text-align:left;'>
+        <p style='font-size:14px; color:#2ECC71; text-align:center; font-weight:600'>
+            Supports Panel Regression for Different Datasets:<br><br>
+            🎯 <strong>Companies</strong><br>
+            📝 <strong>Countries</strong><br>
+            ⏱️ <strong>Industries</strong><br>
+            👇 <strong>Any panel data structure</strong><br>
         </p>
         
     </div>
